d5b3d0e69ae67a279a6e6c34fe4c27a168dc63a7
한톨 <eunlyuing@gmail.com>
2024-05-02 20:53:04 +0900

    feat: Xcode 프로젝트 생성

---
+++ StudentMealRecord/StudentMealRecord/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentMealRecord
+//
+//  Created by 김민준 on 5/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
